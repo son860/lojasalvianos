baaf3e883468768c961228073b71527e07c804e3
Emerson Garcia <emersomggarcia86@gmail.com>
2017-03-16 14:54:42 -0300

    Primeiro Commit
    Primeiro Commit da Loja

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Bungee|Lato" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Husky On Live Commerce</title>
+    </head>
+
+    <body>
+        <!-- Topo principal da loja -->
+        <header id="topo-principal">
+          <!-- Parte com os principais links úteis de acesso da loja -->
+          <article id="top-links-infos">
+            <div class="tli-flex">
+              <ul>
+                <li><a href="#">Lista de Desejos</a></li>
+                <li><a href="#">Atendimento</a></li>
+                <li><a href="#">Meu Carrinho</a></li>
+              </ul>
+            </div>
+
+            <!-- Data e hora atual -->
+            <div id="date-time">
+              <a href="#" class="date-time">Hoje, 07 de Março de 2017.</a>
+            </div>
+          </artcile>
+
+          <!-- Parte com o logo e menu -->
+          <figure id="logo-menu-infos">
+            <div id="logo-flex-area"  class="tli-flex">
+              <img src="img/logo.png" class="logo">
+              <a href="#" class="menu-modular">Sobre Nós</a>
+              <p class="text-call">Olá visitnte! <a href="#" class="text-call-link">Cadastre-se</a> ou faça <a href="#" class="text-call-link">login.</a></p>
+            </div>
+          </figure>
+
+          <!-- Informações Importantes -->
+          <figure id="import-infos">
+            <div id="infos-area" class="tli-flex">
+              <img src="img/img-infos.png" class="img-infos">
+            </div>
+          </figure>
+
+          <!-- Search -->
+          <article id="search">
+            <div id="search-area" class="tli-flex">
+              <input type="search" class="search" placeholder="O que procura?"> 
+              <a href="#" class="btn-search">OK</a>
+            </div>
+          </artcile>
+
+          <!-- Slider -->
+          <figure id="slider">
+            <!-- Slide Group 1 -->
+            <div id="slide-group-1">
+              <a href="#"><img src="img/slider-1.png" class="img-slider"></a>
+            </div>
+
+            <!-- Slide Group 2 -->
+            <div id="slide-group-2">
+             
+            </div>
+
+            <!-- Slide Group 3 -->
+            <div id="slide-group-1">
+              
+            </div>
+          </figure>
+
+      </header>
+
+      <!-- THUMBS BUSCA RÁPIDA -->
+      <section id="busca-rapida-icon">
+        <div id="busca-icons" class="tli-flex">
+            <img src="img/b-icons-1.png" class="busca-icons">
+            <img src="img/b-icons-2.png" class="busca-icons">
+
+            <video id="video">
+            </video>
+        </div>
+      </section>
+ 
+      <!-- INFOS PRINCIPAL GROUP -->
+      <section id="infos-principal-group" class="tli-flex">
+        <!-- MENU menu-categorias -->
+        <aside id="menu-categorias">
+
+          <div id="menu-categorias-title">
+            <p class="menu-cateortias-title">Categorias</p>
+          </div>
+
+          <nav id="menu-categorias">
+             <ul>
+               <li><a href="#">Notebooks</a></li>
+               <li><a href="#">Ultrabooks</a></li>
+               <li><a href="#">Tablets</a></li>
+               <li><a href="#">Periféricos</a></li>
+               <li><a href="#">Fontes</a></li>
+               <li><a href="#">Bloqueadores</a></li>
+             </ul>
+          </nav>
+        </aside>
+
+      </section>
+
+
+    </body>
+</html>
